1b9df9884593943ff23b015417bcee3b1386c4b6
kirillchacha <94245296+kirillchacha@users.noreply.github.com>
2021-11-13 21:41:38 +0200

    new

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+
+let project_folder="dist";
+let source_folder="#projects";
+
+let path={
+build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/", 
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+},
+    src: {
+    html: source_folder + "/",
+    css: source_folder + "/assets/scss/",
+    js: source_folder + "/assets/js/",
+    img: source_folder + "/img/",
+    fonts: source_folder + "/fonts/",
+    }
+}
